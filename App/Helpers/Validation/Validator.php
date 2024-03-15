ff7e5789af4e0c7457753b7f4e400f9748f91d31
Chelali Bavarische <chelali.bavarische@gmail.com>
2024-03-15 09:53:05 +0100

    Correct LoginValidate function logic in validator

--- App/Helpers/Validation/Validator.php
+++ App/Helpers/Validation/Validator.php
@@ -13,37 +13,15 @@ class Validator extends Validation
     public function LoginValidate($inputData)
     {
         $inputData = $this->sanitizeInput($inputData);
-        $allowed_fields = ['email', 'mot_passe'];
+        $allowed_fields = ['username', 'password'];
         $inputData = array_intersect_key($inputData, array_flip($allowed_fields));
         $inputData = $this->sanitizeInput($inputData);
         $rules = [
-            'email' => 'required|email|max:255',
-            'mot_passe' => 'required|min:4',
+            'username' => 'required|min:4|max:255',
+            'password' => 'required|min:4',
         ];
-
         return $this->validate($inputData, $rules);
     }
-
-    public function SignUpValidate($inputData)
-    {
-        $inputData = $this->sanitizeInput($inputData);
-        $allowed_fields = ['email', 'mot_passe','nom','prenom','date_naissance','ville','telephone'];
-        $inputData = array_intersect_key($inputData, array_flip($allowed_fields));
-        $inputData = $this->sanitizeInput($inputData);
-        $rules = [
-            'email' => 'required|email|max:255',
-            'mot_passe' => 'required|min:4',
-            'nom' => 'required|max:50',
-            'prenom' => 'required|max:50',
-            'date_naissance'=>'required|date|max:10',
-            'telephone'=>'required|num|max:10',
-            'ville'=>'required|num|max:10',
-        ];
-
-        return $this->validate($inputData, $rules);
-    }
-
-
 
 
     
