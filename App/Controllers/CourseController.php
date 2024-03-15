d933eef4d5a8837ce42e970e9c3574851c0a49a1
Chelali Bavarische <chelali.bavarische@gmail.com>
2024-03-15 10:01:34 +0100

    Implement "create course" endpoint in CoursesController

--- App/Controllers/CourseController.php
+++ App/Controllers/CourseController.php
@@ -6,25 +6,45 @@ namespace App\Controllers;
 
 use App\Http\Request;
 use App\Http\Response;
+use App\Models\Course;
 use App\Helpers\ViewManager;
 use App\Controllers\Controller;
-
+use App\Helpers\SessionManager;
+use App\Exceptions\DatabaseException;
 
 class CourseController extends Controller
 {
 
    public function index()
    {
-      $body = ViewManager::renderView('test');
-      //$body = ViewManager::renderView('web.users', compact('object1', 'object2'));
-      return  new Response($body);
+      if (SessionManager::get('user_type') === 'Student') {
+         return $this->showCourseForStudent();
+      } elseif (SessionManager::get('user_type') === 'Teacher') {
+         return $this->showCourseForTeacher();
+      }
    }
 
+   private function showCourseForStudent() {
+      
+   }
 
-   public function redirect(Request $request)
+   private function showCourseForTeacher() {
+      
+   }
+
+   public function create(Request $request)
    {
-      var_dump($request->data());
-      return (new Response())->redirect(BASE_URL . '/test');
+      $data=$request->data();
+      $data['teacher_id'] = SessionManager::get('user_id');
+      try {
+         $course = (new Course())->create($data);
+         if ($course) {
+            return new Response("course created successfully ");
+         }
+      } catch (DatabaseException $e) {
+         return new Response("Error Creating a course: " . $e->getMessage());
+      }
+
    }
 
 }
