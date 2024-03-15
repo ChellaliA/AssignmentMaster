81d7fd04ec6d0a8eebe60588e79d635462589165
Chelali Bavarische <chelali.bavarische@gmail.com>
2024-03-15 10:02:22 +0100

    Implement "create assignment" endpoint in AssignmentsController

--- App/Controllers/AssignmentController.php
+++ App/Controllers/AssignmentController.php
@@ -6,9 +6,12 @@ namespace App\Controllers;
 
 use App\Http\Request;
 use App\Http\Response;
+use App\Models\Course;
 use App\Helpers\ViewManager;
 use App\Controllers\Controller;
-
+use App\Helpers\SessionManager;
+use App\Exceptions\DatabaseException;
+use App\Models\Assignment;
 
 class AssignmentController extends Controller
 {
@@ -21,6 +24,20 @@ class AssignmentController extends Controller
    }
 
 
+   public function create(Request $request)
+   {
+      $data=$request->data();
+      $data['course_id'] = $request->id();
+      try {
+         $Assignment = (new Assignment())->create($data);
+         if ($Assignment) {
+            return new Response("Assignment created successfully ");
+         }
+      } catch (DatabaseException $e) {
+         return new Response("Error Creating an Assignment: " . $e->getMessage());
+      }
+   }
+   
    public function redirect(Request $request)
    {
       var_dump($request->data());
